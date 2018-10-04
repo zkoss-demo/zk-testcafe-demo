17fbad9b150b7403c605895e8a5afb5fae7b9d8b
Robert Wenzel <robertwenzel@potix.com>
2018-10-04 11:07:35 +0800

    workaround for https://github.com/DevExpress/testcafe/issues/2763

--- test1.js
+++ test1.js
@@ -1,4 +1,4 @@
-import {Selector} from 'testcafe';
+import {Selector, ClientFunction} from 'testcafe';
 fixture `ZK-Web-Test`.page `https://www.zkoss.org/zkdemo/shadow_elements/step_bar`;
 const addCarButton = Selector('button.stepbar-button').withText('Yes, I need a Car!');
 const backButton = Selector('button.stepbar-button').withText('Back');
@@ -7,13 +7,19 @@ const pageTitle = Selector('.page-title');
 const steps = Selector('.stepbar .step');
 const hasPageTitle = title => pageTitle.withText(title).exists;
 
+// workaround for https://github.com/DevExpress/testcafe/issues/2763 
+// avoid jumping button due to height changes from loading images
+const fixStepbarContentHeight = ClientFunction(() => { document.querySelector('.stepbar-content').style.height = '200px' });
+
 test('ZK demo: step bar example test', async t => {
     await t.expect(hasPageTitle('Destination')).ok()
         .expect(backButton.exists).notOk('Back button should not exist')
         .expect(addCarButton.exists).ok('Add car button should exist')
-        .expect(steps.count).eql(5, 'There should be 5 steps initially')
-        
-        .click(addCarButton)
+        .expect(steps.count).eql(5, 'There should be 5 steps initially');
+    
+    await fixStepbarContentHeight(); //call workaround
+    
+    await t.click(addCarButton)
         .expect(addCarButton.exists).notOk("Add car button should disappear")
         .expect(steps.count).eql(6, 'There should be 6 steps')
         
